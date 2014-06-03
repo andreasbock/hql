00184e25b009af9e95411efcce54d3ff3b0847fa
Andreas Bock <bock@andreasbock.dk>
2014-06-03 11:14:06 +0200

    correction to binomial model

--- src/Instruments/Utils/BinomialModel.hs
+++ src/Instruments/Utils/BinomialModel.hs
@@ -7,20 +7,23 @@
 -- Types and functions for working with binomial models for option pricing
 module Instruments.Utils.BinomialModel where
 
-type OnePeriodBM = (Double, Double, Double) -- up, down, rate modifier
-type D_Up = Double                          -- price @ t+1 up
-type D_Down = Double                        -- price @ t+1 down
+type Value = Double
+type OnePeriodBM = (Double, Double, Double) -- value, up, down, rate modifier
 
 data BinomialModel = Node      OnePeriodBM BinomialModel BinomialModel
-                   | Singleton OnePeriodBM D_Up D_Down
+                   | Singleton OnePeriodBM Value
 
 bm_rec :: BinomialModel -> Double
-bm_rec (Singleton b@(_,_,r) d_up d_dn) = (q*d_up+(1-q)*d_dn)/r
+bm_rec (Singleton b@(u,d,r) v) = (q*d_up+(1-q)*d_dn)/r
   where q = mtgl b
-bm_rec (Node b@(_,_,r) bmu bmd) = (q*d_up+(1-q)*d_dn)/r
-  where q = mtgl b
-        d_up = bm_rec bmu
-        d_dn = bm_rec bmd
+        d_up = v*u
+        d_dn = v*d
 
+bm_rec (Node b@(u,d,r) bmu bmd) = (q*d_up+(1-q)*d_dn)/r
+  where q = mtgl b
+        d_up = u * bm_rec bmu
+        d_dn = d * bm_rec bmd
+
+-- Compute q measure
 mtgl :: (Double, Double, Double) -> Double
 mtgl (u,d,r) = (r-d)/(u-d)

666f440f8eac518c41cfbd4ffb184d1acd7e575f
Andreas Bock <bock@andreasbock.dk>
2014-06-14 14:18:38 +0200

    binomial model using cash type

--- src/Instruments/Utils/BinomialModel.hs
+++ src/Instruments/Utils/BinomialModel.hs
@@ -6,23 +6,24 @@
 --
 -- Types and functions for working with binomial models for option pricing
 module Instruments.Utils.BinomialModel where
+import Utils.Currency
 
-type Value = Double
+type Value = Cash
 type OnePeriodBM = (Double, Double, Double) -- value, up, down, rate modifier
 
 data BinomialModel = Node      OnePeriodBM BinomialModel BinomialModel
                    | Singleton OnePeriodBM Value
 
-bm_rec :: BinomialModel -> Double
-bm_rec (Singleton b@(u,d,r) v) = (q*d_up+(1-q)*d_dn)/r
+bm_rec :: BinomialModel -> Cash
+bm_rec (Singleton b@(u,d,r) v) = scale (recip r) $ d_up + d_dn
   where q = mtgl b
-        d_up = v*u
-        d_dn = v*d
+        d_up = scale (q*u) v
+        d_dn = scale ((1-q)*d) v
 
-bm_rec (Node b@(u,d,r) bmu bmd) = (q*d_up+(1-q)*d_dn)/r
+bm_rec (Node b@(u,d,r) bmu bmd) = scale (recip r) $ d_up + d_dn
   where q = mtgl b
-        d_up = u * bm_rec bmu
-        d_dn = d * bm_rec bmd
+        d_up = scale (q*u)     $ bm_rec bmu
+        d_dn = scale ((1-q)*d) $ bm_rec bmd
 
 -- Compute q measure
 mtgl :: (Double, Double, Double) -> Double

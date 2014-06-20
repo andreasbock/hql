09376a48e453bd8e4e8fe3a24e385948a91cea58
Andreas Bock <bock@andreasbock.dk>
2014-06-20 20:27:43 +0200

    maybe monad to binomial if \not\exists Q-measure

--- src/Instruments/Utils/BinomialModel.hs
+++ src/Instruments/Utils/BinomialModel.hs
@@ -7,6 +7,8 @@
 -- Types and functions for working with binomial models for option pricing
 module Instruments.Utils.BinomialModel where
 import Utils.Currency
+import Debug.Trace
+import Control.Applicative
 
 type Value = Cash
 type OnePeriodBM = (Double, Double, Double) -- value, up, down, rate modifier
@@ -14,17 +16,28 @@ type OnePeriodBM = (Double, Double, Double) -- value, up, down, rate modifier
 data BinomialModel = Node      OnePeriodBM BinomialModel BinomialModel
                    | Singleton OnePeriodBM Value
 
-bm_rec :: BinomialModel -> Cash
-bm_rec (Singleton b@(u,d,r) v) = scale (recip r) $ d_up + d_dn
+bm_rec :: BinomialModel -> Maybe Cash
+bm_rec (Singleton b@(u,d,r) v) =
+  let
+    q_sum = (+) <$> d_up <*> d_dn
+  in scale (1/r) <$> q_sum
   where q = mtgl b
-        d_up = scale (q*u) v
-        d_dn = scale ((1-q)*d) v
+        d_up = scale_q           q  u v
+        d_dn = scale_q ((1-) <$> q) d v
 
-bm_rec (Node b@(u,d,r) bmu bmd) = scale (recip r) $ d_up + d_dn
-  where q = mtgl b
-        d_up = scale (q*u)     $ bm_rec bmu
-        d_dn = scale ((1-q)*d) $ bm_rec bmd
+bm_rec (Node b@(u,d,r) bmu bmd) =
+  do
+    q    <- mtgl b
+    d_up <- fmap (scale $ q*u)     $ bm_rec bmu
+    d_dn <- fmap (scale $ (1-q)*d) $ bm_rec bmd
+    return $ scale (1/r) $ d_up + d_dn
 
--- Compute q measure
-mtgl :: (Double, Double, Double) -> Double
-mtgl (u,d,r) = (r-d)/(u-d)
+-- | Compute Q measure
+mtgl :: OnePeriodBM -> Maybe Double
+mtgl (u,d,r) | 0 <= q && q <= 1 = Just q
+              | otherwise = trace "No equivalent martingale measure!" Nothing
+  where q =  (r-d)/(u-d)
+
+scale_q :: Maybe Double -> Double -> Cash -> Maybe Cash
+scale_q  Nothing c v = Nothing
+scale_q (Just f) c v = pure $ scale (f*c) v
